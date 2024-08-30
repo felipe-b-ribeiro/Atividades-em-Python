5e35a441da3da016288a305e145f2e350c883603
Felipe Bitencourt Ribeiro <160271031+felipe-b-ribeiro@users.noreply.github.com>
2024-08-30 08:03:38 -0300

    Add files via upload

---
+++ transformargraus.py
@@ -0,0 +1,4 @@
+celsius = int(input('Digite uma temperatura em graus C°: '))
+fahrenheint = (celsius * 1.8) + 32
+print('\n')
+print('A temperatura de', celsius , 'C° é equivalente à', fahrenheint , 'F°.')
